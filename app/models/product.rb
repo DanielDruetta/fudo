53b6de36bd3c584e6992c0eef3158fed6a9745b9
Daniel Druetta <daniel.druetta@web-experto.com.ar>
2025-08-23 16:49:45 -0300

    add initializers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,27 +1,23 @@
-require 'redis'
 require 'json'
+require_relative '../../config/initializers/redis'
 
 class Product
-  def self.redis
-    @redis ||= Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/0')
-  end
-
   def self.create(name)
-    id = redis.incr('product:id')
-    redis.hset('products', id, { id: id, name: name }.to_json)
+    id = REDIS.incr('product:id')
+    REDIS.hset('products', id, { id: id, name: name }.to_json)
     { id: id, name: name }
   end
 
   def self.all
-    redis.hvals('products').map { |p| JSON.parse(p) }
+    REDIS.hvals('products').map { |p| JSON.parse(p) }
   end
 
   def self.update(id, name)
-    prod_json = redis.hget('products', id)
+    prod_json = REDIS.hget('products', id)
     return nil unless prod_json
     prod = JSON.parse(prod_json)
     prod['name'] = name
-    redis.hset('products', id, prod.to_json)
+    REDIS.hset('products', id, prod.to_json)
     prod
   end
 end
--- app/product_store.rb
+++ app/product_store.rb
@@ -1,18 +1,14 @@
-require 'redis'
 require 'json'
+require_relative '../config/initializers/redis'
 
 class ProductStore
-  def self.redis
-    @redis ||= Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379/0')
-  end
-
   def self.add(name)
-    id = redis.incr('product:id')
-    redis.hset('products', id, { id: id, name: name }.to_json)
+    id = REDIS.incr('product:id')
+    REDIS.hset('products', id, { id: id, name: name }.to_json)
     id
   end
 
   def self.all
-    redis.hvals('products').map { |p| JSON.parse(p) }
+    REDIS.hvals('products').map { |p| JSON.parse(p) }
   end
 end
